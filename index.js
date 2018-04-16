2bc8b9cbe7fde4bdcc97c7d81889d44c449b2897
Vichyssoise <v@vichyssoise.io>
2018-04-17 02:32:05 +0900

    Improve entangle chaining

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const pick = arr => arr[Math.floor(Math.random() * arr.length)]
+const pick = arr => () => arr[Math.floor(Math.random() * arr.length)]
 
 const State = Object.freeze({
-  SUPERPOSED: 'Superposed',
-  OBSERVED: 'Observed',
+  Uncertain: 'Uncertain',
+  Measured: 'Measured',
 })
 
 class Uncertainty {
-  constructor(resolver = () => pick([true, false]), dependency) {
-    this._state = State.SUPERPOSED
+  constructor(arg = [true, false], dependency) {
+    this._dependency = dependency
+    this._resolver = (() => {
+      if (arg instanceof Array) {
+        return pick(arg)
+      } else if (arg instanceof Function) {
+        return arg
+      } else {
+        throw TypeError(`Uncertainty resolver ${arg} is not an array`)
+      }
+    })()
+    this._state = State.Uncertain
     this._value = null
     this._chain = dependency ? dependency._chain : []
     this._chain.push(this)
 
     Object.defineProperty(this, 'value', {
       get: () => {
-        if (this._state === State.SUPERPOSED) {
-          this._value = resolver(dependency ? dependency.value : null)
-          this._state = State.OBSERVED
-          this._chain.map(e => e.value)
+        if (this._state === State.Uncertain) {
+          while(this._chain.length > 0) {
+            const e = this._chain.pop()
+            e._value = e._resolver(e._dependency ? e._dependency.value : null)
+            e._state = State.Measured
+          }
         }
 
         return this._value
