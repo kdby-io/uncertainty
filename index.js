b10afa830d82b6a698cb7b554f9933c436730856
Vichyssoise <v@vichyssoise.io>
2018-04-16 09:53:44 +0900

    Add state

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const pick = arr => arr[Math.floor(Math.random() * arr.length)]
 
+const State = Object.freeze({
+  SUPERPOSED: 'Superposed',
+  OBSERVED: 'Observed',
+})
+
 class Uncertainty {
   constructor(resolver = () => pick([true, false]), dependency) {
-    this._value = 'superposed'
-    this.chain = dependency ? dependency.chain : []
-    this.chain.push(this)
+    this._state = State.SUPERPOSED
+    this._value = null
+    this._chain = dependency ? dependency._chain : []
+    this._chain.push(this)
 
     Object.defineProperty(this, 'value', {
       get: () => {
-        if (this._value === 'superposed') {
+        if (this._state === State.SUPERPOSED) {
           this._value = resolver(dependency ? dependency.value : null)
-          this.chain.map(e => e.value)
+          this._state = State.OBSERVED
+          this._chain.map(e => e.value)
         }
 
         return this._value
@@ -18,7 +25,7 @@ class Uncertainty {
     })
   }
   
-  peek() { return this._value }
+  peek() { return this._state }
   valueOf() { return this.value }
   toString() { return this.value }
   inspect() { return `[Uncertainty: ${this.value}]` }
